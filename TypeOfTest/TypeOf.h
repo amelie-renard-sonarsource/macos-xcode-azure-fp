b32673b9331d8924e571bc887f397cd89e7a1844
Amelie Renard <amelie.renard@sonarsource.com>
2021-06-30 15:43:26 +0200

    Simplify

--- TypeOfTest/TypeOf.h
+++ TypeOfTest/TypeOf.h
@@ -1,21 +1,5 @@
-//
-//  TypeOf.h
-//  TypeOfTest
-//
-//  Created by saix on 2021/6/29.
-//
-
-#import <Foundation/Foundation.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface TypeOf : NSObject
-
-@property (nonatomic) int count;
+@interface TypeOf
 
 -(void)typeofTest;
 
-
 @end
-
-NS_ASSUME_NONNULL_END
--- TypeOfTest/TypeOf.m
+++ TypeOfTest/TypeOf.m
@@ -1,19 +1,9 @@
-//
-//  TypeOf.m
-//  TypeOfTest
-//
-//  Created by saix on 2021/6/29.
-//
-
 #import "TypeOf.h"
 
 @implementation TypeOf
 
 -(void)typeofTest {
     __weak typeof(self) weakSelf = self;
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        weakSelf.count++;
-    });
 
     __weak __typeof__(self) weakSelf1 = self;
 
